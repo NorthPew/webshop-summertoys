393c07cc03246474bc895918a0694842d74f6d06
NorthPew <vbergstrom02@gmail.com>
2023-05-06 15:54:10 +0200

    Now you are able to search for specific products

--- src/routes/Products.jsx
+++ src/routes/Products.jsx
@@ -1,5 +1,9 @@
 import { useLoaderData, Link } from "react-router-dom";
 import AddToCartButton from "./components/AddToCartButton";
+import styled from "styled-components";
+import react, { useState } from "react";
+
+// Temp data
 
 const tempData = [
         {
@@ -25,33 +29,94 @@ const tempData = [
         }
     ]
 
+
 export const loader = () => tempData
+
+// Styled
+
+// Wrapper
+
+const Wrapper = styled.section `
+    width: 80vw;
+    height: 100%;
+    gap: 15px;
+    display: grid;
+`
+
+// Title & Input Fields (Multi container)
+
+const TopMultiContainer = styled.div `
+    width: 100%;
+    height: 64px;
+    border-bottom: 1px solid #000;
+    display: flex;
+    flex-flow: column wrap;
+    justify-content: center;
+
+`
+
+const PageTitle = styled.h1 `
+    font-size: 38px;
+    margin: 0px;
+`
+
+const TopSideContainer = styled.div `
+    width: 50vw;
+    flex-flow: row-reverse wrap;
+    display: flex;
+`
 
 function Products() {
     const productData = useLoaderData()
+    const [searchValue, setSearchValue] = useState('');
+    const handleSearch = (event) => {
+        setSearchValue(event.target.value);
+      };
     
+      const filteredProducts = productData.filter((product) =>
+        product.name.toLowerCase().includes(searchValue.toLowerCase())
+  );
     return (
-        <section>
-            <h1>Våra produkter</h1>
-            <p>Vill du ha kul i solen? Här hittar du alla våra sommarleksprodukter som är perfekta för stranden, trädgården eller parken.</p>
-            <p>Vi har allt från vattenpistoler och till badbollar. Oavsett om du vill leka eller av koppla av har vi något för dig.</p>  
-            <p>Beställ idag och få leverans inom några dagar!</p>
-
+        <Wrapper>
+            <TopMultiContainer>
+                <PageTitle>Produkter</PageTitle>
+                <TopSideContainer>
+                    <input type="text" placeholder="Sök" onChange={handleSearch}></input>
+                    <select>
+                        <option>Rekommendera</option>
+                        <option>Namn</option>
+                        <option>Lågt pris</option>
+                        <option>Högst pris</option>
+                    </select>
+                    <p>Sortera efter</p>
+                </TopSideContainer>
+            </TopMultiContainer>
             <ul>
                 {
-                productData.map((product) => (
+                filteredProducts ? filteredProducts.map((product) => (
                     <li key={product.id}> 
                         <Link to={'/products/' + product.id}>
                             <h2>{product.name}</h2>
                             <img src={product.image} alt={product.name} />
                             <p>{product.description}</p>
-                            <p>{product.price}</p>
+                            <p>{product.price} kr</p>
                         </Link>
                         <AddToCartButton product={product} />
                     </li>
+                )) :
+                productData.map((product) => (
+                    <li key={product.id}> 
+                    <Link to={'/products/' + product.id}>
+                        <h2>{product.name}</h2>
+                        <img src={product.image} alt={product.name} />
+                        <p>{product.description}</p>
+                        <p>{product.price} kr</p>
+                    </Link>
+                    <AddToCartButton product={product} />
+                </li>
                 ))}
             </ul>
-        </section>
+        </Wrapper>
     )
 }
 
