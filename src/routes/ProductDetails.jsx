9a366a6a616eb4fdc8b94d043d3d6275575de8b6
NorthPew <vbergstrom02@gmail.com>
2023-05-07 14:05:02 +0200

    Added nearly all stuff that are needed for product view. Saving my work so far

--- src/routes/ProductDetails.jsx
+++ src/routes/ProductDetails.jsx
@@ -1,4 +1,59 @@
 import { useParams, useLoaderData, Link, useNavigate } from "react-router-dom"
+import styled from "styled-components"
+import AddToCartButton from "./components/AddToCartButton"
+
+
+// Styled
+
+// Wrapper
+
+const Wrapper = styled.section `
+    width: 85vw;
+    height: 100%;
+    gap: 15px;
+    display: grid;
+`
+
+// Title
+
+const TitleContainer = styled.div `
+    width: 100%;
+    height: 64px;
+    border-bottom: 1px solid #000;
+    display: flex;
+    flex-flow: column wrap;
+    justify-content: center;
+
+`
+
+const PageNavigation = styled.p `
+    font-size: 16px;
+    font-weight: 400;
+    margin: 0px;
+`
+
+const PageLink = styled(Link) `
+    color: #242424;
+    &:link {
+        color: #242424;
+    }
+    &:active {
+        color: #242424;
+    }
+`
+
+const PageLinkBox = styled.div `
+    display: flex;
+    flex-flow: row wrap;
+    align-items: center;
+    column-gap: .65em;
+`
+
+const ProductImage = styled.img `
+    width: 605px;
+    max-height: 750px;
+`
+
 
 function ProductDetails() {
     const navigate = useNavigate()
@@ -18,9 +73,17 @@ function ProductDetails() {
     }
 
     return (
-        <section>
-            <h1>{product.name}</h1>
-        </section>
+        <Wrapper>
+            <TitleContainer>
+                <PageNavigation>
+                    <PageLinkBox><PageLink to='/'>Hem</PageLink> &gt; <PageLink to='/products'>Produkter</PageLink> &gt; <p>{product.name}</p>  </PageLinkBox>
+                </PageNavigation>
+            </TitleContainer>
+            <ProductImage src={product.image} alt={product.name} />
+            <p>{product.name}</p>
+            <p>{product.price} kr</p>
+            <AddToCartButton product={product} />
+        </Wrapper>
     )
 }
 
--- src/routes/components/AddToCartButton.jsx
+++ src/routes/components/AddToCartButton.jsx
@@ -1,10 +1,20 @@
 import { useContext } from "react";
 import { CartContext } from "../../Wrapper";
+import styled from "styled-components";
+
+const AddToCartBtn = styled.button `
+  color: #fff;
+  background-color: #242424;
+  border-radius: 0px;
+  &:hover {
+    color: #10a7dd;
+  }
+`
 
 function AddToCartButton({product}) {
     const { addToCart } = useContext(CartContext);
     return (
-      <button onClick={() => addToCart(product)}>Add to cart</button>
+      <AddToCartBtn onClick={() => addToCart(product)}>Lägg till i varukorgen</AddToCartBtn>
     )
 }
 
